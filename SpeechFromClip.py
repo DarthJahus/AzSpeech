53afb19417dbce26d373baf1caa1c89e408e5cd4
Jahus <jahus@subspace.club>
2024-06-03 18:32:06 +0200

    Add better options for file output. Update readme.md

--- SpeechFromClip.py
+++ SpeechFromClip.py
@@ -2,10 +2,20 @@ from azure.cognitiveservices.speech import SpeechConfig, audio, SpeechSynthesize
 import pyperclip as clipboard
 import json
 from winsound import Beep
+from subprocess import call
+from platform import system
+from os import startfile
+from datetime import datetime
 
 
 with open("config.json", 'r', encoding="utf-8") as _f:
     __config = json.loads(_f.read())
+    if "OUT_FILE" not in __config:
+        __config["OUT_FILE"] = False
+    elif __config["OUT_FILE"] == True:  # check if True, not if not False:
+        __config["OUT_FILE"] = "AzSpeech %s.wav" % datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    if "READ_FILE" not in __config:
+        __config["READ_FILE"] = False
 
 
 speech_config = SpeechConfig(
@@ -24,10 +34,23 @@ speech_synthesizer = SpeechSynthesizer(
 )
 
 
+def open_file(file):
+    # cf. https://stackoverflow.com/questions/434597
+    if system() == 'Darwin':  # macOS
+        call(('open', file))
+    elif system() == 'Windows':  # Windows
+        startfile(file)
+    else:  # linux variants
+        call(('xdg-open', file))
+    pass
+
+
 if __name__ == "__main__":
     _speech = speech_synthesizer.speak_text_async(clipboard.paste().strip()).get()
     if _speech.reason == ResultReason.SynthesizingAudioCompleted:
         print("Speech synthesized.")
+        if __config["READ_FILE"] and __config["OUT_FILE"]:
+            open_file(__config["OUT_FILE"])
         exit(0)
     elif _speech.reason == ResultReason.Canceled:
         print("Speech synthesis canceled: {}".format(_speech.cancellation_details.reason))
--- readme.md
+++ readme.md
@@ -12,5 +12,21 @@ A GUI is available, if using clipboard is not what you want.
         "SPEECH_KEY": "bdxxxxxxxxxxxxxxxxxxxxxxxxxxxxa5",
         "SPEECH_REGION": "eastus",
         "SPEECH_VOICE": "en-US-EmmaMultilingualNeural",
-        "OUT_FILE": false
+        "OUT_FILE": false,
+        "READ_FILE": false
     }
+
+- `SPEECH_KEY`: Your Azure service key.
+- `SPEECH_REGION`: Region your Azure service runs in.
+- `SPEECH_VOICE`: Voice name.
+- `OUT_FILE`:
+  - if `false`, the speech will be read through the default sound device;
+  - You can set `OUT_FILE` to the name of the output file (example: `rec.wav`);
+  - if `true`, the speech will be saved to a file named `AzSpeech %Y-%m-%d_%H-%M-%S.wav` (example: `AzSpeech 2024-06-03_18-22-04.wav`).
+- `READ_FILE`: set `true` if you want the saved file to be opened with the default program for `.wav` format (example: Media Player).
+
+Note:
+ - In `.res/azure.json`, you will find a list of regions (this list may be outdated. Make sure to set the correct region for your service).
+ - `.res/azure.json` lists some Multilingual Neural voices. These are the *best*, and the most fit for most languages.
+ - Microsoft Azure offers many other voices. Check out [Language and voice support for the Speech service](https://learn.microsoft.com/en-us/azure/ai-services/speech-service/language-support?tabs=tts) on Microsoft Learn for a comprehensive list.
+ - In a production environment, you should use the API to get a list of regions / voices, instead of a static file.
